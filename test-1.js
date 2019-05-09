a884453cf6e25ce43d171164177f707c3a876da1
kimbeomgyu <qjarb1994@gmail.com>
2019-05-09 16:12:32 +0900

    test-1 수정-1

--- test-1.js
+++ test-1.js
@@ -1,13 +1,10 @@
 // 1. 아래 코드의 의미를 설명하고 ES6의 functional programming을 활용해서 변경하세요.
 const incomes = [1000, 2000, 3000, 4000];
 
-let vats = [];
-for (i = 0; i < incomes.length; i++) {
-  // 부가세 계산
-  vats[i] = incomes[i] / 11;
-}
+let vats = incomes.map(income => income / 11);
+let price = incomes.reduce((A, income) => A + income, 0);
 
-let price = 0;
-for (let i in incomes) {
-  price += incomes[i];
-}
+// 부가세 계산하는 식으로써 각각의 수입을 순회하면서
+// vats라는 새로운 배열에 각각의 값들을 / 11 하여 담습니다.
+
+// 수입을 모두 더하는 식입니다.
