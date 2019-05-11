1b6ed09d9460bf574b59b469efd2ecc033899548
kimbeomgyu <qjarb1994@gmail.com>
2019-05-11 16:58:49 +0900

    App.js state값, 오류 수정

--- frontend-interview/src/components/App.js
+++ frontend-interview/src/components/App.js
@@ -6,22 +6,18 @@ import { bindActionCreators } from "redux";
 import * as calcActions from "../store/modules/Calc";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      multiply: 12,
-      substract: null
-    };
-  }
+  state = {
+    multiply: 12,
+    substract: null
+  };
 
   componentDidMount() {
     this.init();
   }
 
   _handleDetecting = () => {
-    this.setState({
-      plus: 30
-    });
+    this.setState({ plus: 33 });
+    //기존의 코드를 유지하되 값을 변경하였습니다.
   };
 
   _handleShow = val => {
@@ -34,9 +30,11 @@ class App extends Component {
     }
   };
 
-  setStating() {
+  setStating({ sNumber }) {
+    // 첫번째 인자로 들어온 객체안에 sNumber가 있다면 담도록 변경하였습니다.
     return {
-      sNumber: 13,
+      sNumber: sNumber || 13,
+      // sNumber이 없다면 기존의 13의 값을 유지하도록 수정하였습니다.
       pass: function() {
         var substract;
         return (substract = this.sNumber);
@@ -45,8 +43,10 @@ class App extends Component {
   }
 
   init() {
-    var substract = this.setStating.call({ sNumber: -33 }).pass();
-
+    var substract = this.setStating.call(this, { sNumber: -33 }).pass();
+    // call을 이용한 실행이 불필요하다고 생각되어 제거하려고 하였으나,
+    // 기존의 코드를 삭제하는것보다 추가하여 동작시키는 것이 좋다고 판단하여
+    // this를 통해 실행하도록 하였습니다.
     this.setState({ substract });
   }
 
@@ -54,14 +54,17 @@ class App extends Component {
     var { source, show } = this.props;
     var { multiply, substract, plus } = this.state;
     return (
-      <Express
-        source={source}
-        multiply={multiply}
-        show={show}
-        plus={plus}
-        substract={substract}
-        handleShow={this._handleShow}
-      />
+      //componentDidMount에서 state를 변경하고 난 이후에 view가 그려지도록 수정하였습니다.
+      substract && (
+        <Express
+          source={source}
+          multiply={multiply}
+          show={show}
+          plus={plus}
+          substract={substract}
+          handleShow={this._handleShow}
+        />
+      )
     );
   }
 }
